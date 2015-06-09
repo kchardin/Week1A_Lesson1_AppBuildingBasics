6cb8297c74d021a2463abd223a194f7c1758ea59
kchardin <katharine.c.hardin@gmail.com>
2015-06-09 19:34:29 -0400

    Initial Commit

---
+++ BL_AppTests/BL_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  BL_AppTests.swift
+//  BL_AppTests
+//
+//  Created by Katharine Hardin on 6/9/15.
+//  Copyright (c) 2015 KCH Exchange. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class BL_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
